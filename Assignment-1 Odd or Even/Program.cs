476e1cdb61230a7473e06dd2fa6c1f7198b127cf
Nikhil <nikhilpraveen7777@gmail.com>
2024-06-09 19:43:52 +0530

    Added Assignment-1

---
+++ Assignment-1 Odd or Even/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Assignment_1_Odd_or_Even
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a target number: ");
+            int targetNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Do you want to print odd or even numbers?(odd/even)");
+            string choice = Console.ReadLine().ToLower();
+
+            Console.WriteLine($"Printing {choice} numbers less than {targetNumber}");
+
+            for (int i = (choice == "even" ? 0 : 1); i <= targetNumber; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
